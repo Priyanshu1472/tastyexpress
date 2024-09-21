ba514c93518ffc76fd0ed3e700582b961686120c
MISTRY90 <142777171+MISTRY90@users.noreply.github.com>
2024-09-21 21:55:13 +0530

    basic home page layout

--- tastyexpress/src/App.js
+++ tastyexpress/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './components/home'
+import Footer from './components/footer'
+import Navbar from './components/navbar'
+import Features from './components/features';
+
 
 function App() {
   return (
     <>
-      
+      <Home/>
+      <Features/>
+      <Footer/>
     </>
   );
 }
--- tastyexpress/src/components/home.jsx
+++ tastyexpress/src/components/home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+
 const Home = () => {
     return (
         <>
-            
+
+         <section className="banner">
+         <nav>
+    <ul class="menuItems">
+      <li><a href='#' data-item='Home'>Home</a></li>
+      <li><a href='#' data-item='About'>About</a></li>
+      <li><a href='#' data-item='Projects'>Projects</a></li>
+      <li><a href='#' data-item='Blog'>Blog</a></li>
+      <li><a href='#' data-item='Contact'>Contact</a></li>
+    </ul>
+  </nav>
+          <div className="banner_text">
+            <h2>Welcome to </h2> <br />
+            <h2>TastyExpress</h2>
+          </div>
+         </section>
         </>
     );
 };
